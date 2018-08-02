42d52aac610c107b6e43bec55be556ac0fc5d608
ThersiquelG <gregthersiquel@hotmail.fr>
2018-08-02 10:55:13 +0200

    Programme terminé. Optimisation needed

--- SimplonCoin/src/myPackage/Application.java
+++ SimplonCoin/src/myPackage/Application.java
@@ -1,25 +1,24 @@
 package myPackage;
 import java.util.Scanner;
 
-//ScÈnario d'utilisation d'une appli bancaire avec 1000 euros d'avoir disponible.
 
 public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		//crÈation et inialisation d'une variable "running" de type boolean, initialisÈe en "true" pour la boucle lors de l'utilisation du programme
+
+		//cr√©ation et inialisation d'une variable "running" de type boolean, initialis√©e en "true" pour la boucle lors de l'utilisation du programme
 		boolean running = true;
-		
-		//crÈation d'une instance de Scanner
+
+		//cr√©ation d'une instance de Scanner
 		Scanner sc = new Scanner(System.in);
-		
-		//crÈation d'une instance de la classe enfant "CCourant", hÈritant des propriÈtÈes de la classe parent "Comptes"
+
+		//cr√©ation d'une instance de la classe enfant "CCourant", h√©ritant des propri√©t√©es de la classe parent "Comptes"
 		CCourant CourantGreg = new CCourant();
-		CourantGreg.setSolde(1000);
-		
-		
-		//CrÈation d'une deuxiËme instance de la classe enfant "CCourant", hÈritant des propriÈtÈes de la classe parent "Comptes"
+		CourantGreg.setSolde(0);
+
+
+		//Cr√©ation d'une deuxi√®me instance de la classe enfant "CCourant", h√©ritant des propri√©t√©es de la classe parent "Comptes"
 		PEL DummyAccount = new PEL();
 		DummyAccount.setID(239324);
 		DummyAccount.setNom("Jean-Joseph");
@@ -31,17 +30,17 @@ public class Application {
 		System.out.println("Veuillez votre nom.");
 		String nom = sc.next();
 		CourantGreg.setNom(nom);
-		System.out.println("Veuillez votre prÈnom.");
+		System.out.println("Veuillez votre pr√©nom.");
 		String prenom = sc.next();
 		CourantGreg.setPrenom(prenom);
 		//--------------------------
 		System.out.println("Bon retour parmi nous Mr." + CourantGreg.getNom() + "  " + CourantGreg.getPrenom());
 
 		while(running) {
-			
-			//Choix des opÈrations
-			System.out.println("Quelle(s) opÈration(s) voulez-vous effectuer ?");
-			System.out.println("Veuillez-taper le numÈro de l'opÈration dÈsirÈe.");
+
+			//Choix des op√©rations
+			System.out.println("Quelle(s) op√©ration(s) voulez-vous effectuer ?");
+			System.out.println("Veuillez-taper le num√©ro de l'op√©ration d√©sir√©e.");
 			System.out.println("Consulter mes soldes - 1");
 			System.out.println("***");
 
@@ -51,10 +50,13 @@ public class Application {
 			System.out.println("Versement - 3");
 			System.out.println("***");
 
-			System.out.println("Se deconnecter - 4");
+			System.out.println("D√©position d'argent - 4");
 			System.out.println("***");
-			
-			//crÈation d'une variable de type int pour que l'utilisateur puisse sÈlectionner une opÈration
+
+			System.out.println("Se deconnecter - 5");
+			System.out.println("***");
+
+			//cr√©ation d'une variable de type int pour que l'utilisateur puisse s√©lectionner une op√©ration
 			int actions = sc.nextInt();
 
 
@@ -67,35 +69,43 @@ public class Application {
 				//Retrait d'argent
 			} else if(actions == 2) {
 				System.out.println("Quelle somme souhaitez vous retirer ?");
-				
-				
-				//crÈation d'une variable de type double pour indiquer un montant de retrait
+
+
+				//cr√©ation d'une variable de type double pour indiquer un montant de retrait
 				double amount = sc.nextDouble();
-				
+
 				CourantGreg.retrait(CourantGreg, amount);
-				
-				
+
+
 				//Versement
 			} else if(actions == 3) {
-				
+
 				//Instruction pour l'utilisateur
 				System.out.println("Vers quels compte(s) souhaitez vous transferer de l'argent ?");
 				System.out.println("[1]: " + DummyAccount.getNom() + "  " + DummyAccount.getPrenom() + "  :" + " ID de compte " + DummyAccount.getID());
-				
-				//crÈation d'une variable de type int pour que l'utilisateur puisse sÈlectionner le compte dÈsirÈ
+
+				//cr√©ation d'une variable de type int pour que l'utilisateur puisse s√©lectionner le compte d√©sir√©
 				int x = sc.nextInt();
 				if(x == 1) {
 					System.out.println("Quelle somme souhaitez vous envoyer vers ce compte ?");
-					
-					//crÈation d'une variable de type double pour indiquer le montant de versement dÈsirÈ
+
+					//cr√©ation d'une variable de type double pour indiquer le montant de versement d√©sir√©
 					double versAmount = sc.nextInt();
-					
-					CourantGreg.versement(DummyAccount, versAmount);
+
+					CourantGreg.virement(DummyAccount, versAmount);
 				}
+			}
+
+			//rajouter du cr√©dit
+			else if (actions == 4) {
+				System.out.println("De combien voulez vous cr√©diter votre compte ?");
+				double credit = sc.nextDouble();
+
+				CourantGreg.cr√©dit(CourantGreg, credit);
 
 
 				//Log out
-			} else if(actions == 4) {
+			} else if(actions == 5) {
 				running = false;
 				System.out.println("Au revoir.");
 			}
@@ -106,5 +116,3 @@ public class Application {
 
 
 }
-
-
--- SimplonCoin/src/myPackage/CCourant.java
+++ SimplonCoin/src/myPackage/CCourant.java
@@ -1,26 +1,24 @@
 package myPackage;
 
 
-//PremiËre classe enfant.
+//Premi√®re classe enfant.
 public class CCourant extends Comptes {
-	
-	
 	//Retrait                                               --------------------------------------------------------------------------
-	
+
 	/*
 	 * System.out.println("Quelle somme souhaitez vous retirer ?");
-				
-				
-				//crÈation d'une variable de type double pour indiquer un montant de retrait
+
+
+				//cr√©ation d'une variable de type double pour indiquer un montant de retrait
 				double amount = sc.nextDouble();
-				
-				//OpÈration de retrait qui va attribuer une nouvelle valeur avec .setSolde de la classe CourantGreg
+
+				//Op√©ration de retrait qui va attribuer une nouvelle valeur avec .setSolde de la classe CourantGreg
 				double newsold = CourantGreg.getSolde() - amount ;
 				CourantGreg.setSolde(newsold);
 				CourantGreg.getSolde();
 				//---
-				
-				//vÈrification du solde pour pouvoir effectuer l'opÈration
+
+				//v√©rification du solde pour pouvoir effectuer l'op√©ration
 				if(CourantGreg.getSolde() < 0) {
 					System.out.println("Vous n'avez pas assez d'argent.");
 				} else {
@@ -30,33 +28,30 @@ public class CCourant extends Comptes {
 					System.out.println("----------------------------");
 					running = true;
 	 */
-		public void retrait(CCourant dÈbitÈ, double montant) {
-			
-			if (montant > dÈbitÈ.getSolde()) {
-				System.out.println("Vous n'avez pas assez d'argent");
-			} else {
-				
-				dÈbitÈ.solde = dÈbitÈ.getSolde() - montant ;
-				System.out.println("Vous avez retirÈ : " + montant + " euros.");
-				System.out.println("Il vous reste :" + dÈbitÈ.getSolde() + " euros.");
-			}
-			
+	public void retrait(CCourant d√©bit√©, double montant) {
+
+		if (montant > d√©bit√©.getSolde()) {
+			System.out.println("Vous n'avez pas assez d'argent");
+		} else {
+
+			d√©bit√©.solde = d√©bit√©.getSolde() - montant ;
+			System.out.println("Vous avez retir√© : " + montant + " euros.");
+			System.out.println("Il vous reste :" + d√©bit√©.getSolde() + " euros.");
 		}
-		
-		
-		
-		//Virement                                               --------------------------------------------------------------------------
-		
-		/*
-		 * //vÈrification du solde pour pouvoir effectuer l'opÈration
+
+	}
+	//Virement                                               --------------------------------------------------------------------------
+
+	/*
+	 * //v√©rification du solde pour pouvoir effectuer l'op√©ration
 					if(versAmount > CourantGreg.getSolde()) {
 						System.out.println("Vous n'avez pas assez d'argent.");
 					} else {
-						
-						//OpÈration de versement qui va attribuer une nouvelle valeur avec .setSolde de la classe CourantGreg
+
+						//Op√©ration de versement qui va attribuer une nouvelle valeur avec .setSolde de la classe CourantGreg
 						CourantGreg.setSolde(CourantGreg.getSolde() - versAmount);
 						CourantGreg.getSolde();
-						System.out.println("Vous avez envoyer : " + versAmount + " au compte numÈro" + DummyAccount.getNom() + "  " + DummyAccount.getPrenom() + "  :" + " ID de compte " + DummyAccount.getID());
+						System.out.println("Vous avez envoyer : " + versAmount + " au compte num√©ro" + DummyAccount.getNom() + "  " + DummyAccount.getPrenom() + "  :" + " ID de compte " + DummyAccount.getID());
 						System.out.println("----------------------------");
 						System.out.println("Il vous reste :"  + (CourantGreg.getSolde()) + "euros");
 						System.out.println("----------------------------");
@@ -65,25 +60,32 @@ public class CCourant extends Comptes {
 						System.out.println("Votre ami a maintenant :" + (DummyAccount.getSolde() + versAmount) + " euros.");
 						System.out.println("**************************");
 					}
-		 */
-		
-		public void versement(PEL receveur, double montant) {
-			if(montant > getSolde()) {
-				System.out.println("Vous n'avez pas assez d'argent.");
-			} else {
-				solde = getSolde() - montant ;
-				receveur.solde = receveur.getSolde() + montant;
-				
-				System.out.println("Vous avez envoyer : " + montant + " euros. " +  "au compte numÈro" + receveur.getNom() + "  " + receveur.getPrenom() + "  :" + " ID de compte " + receveur.getID());
-				System.out.println("----------------------------");
-				System.out.println("Il vous reste :"  + (getSolde()) + "euros");
-				System.out.println("----------------------------");
-				//double dummySold = versAmount + DummyAccount.getSolde();
-				System.out.println("**************************");
-				System.out.println("Votre ami a maintenant :" + (receveur.getSolde() + " euros."));
-				System.out.println("**************************");
-			}
-			
-			
+	 */
+
+	public void virement(PEL receveur, double montant) {
+		if(montant > getSolde()) {
+			System.out.println("Vous n'avez pas assez d'argent.");
+		} else {
+			solde = getSolde() - montant ;
+			receveur.solde = receveur.getSolde() + montant;
+
+			System.out.println("Vous avez envoyer : " + montant + " euros. " +  "au compte num√©ro" + receveur.getNom() + "  " + receveur.getPrenom() + "  :" + " ID de compte " + receveur.getID());
+			System.out.println("----------------------------");
+			System.out.println("Il vous reste :"  + (getSolde()) + "euros");
+			System.out.println("----------------------------");
+			//double dummySold = versAmount + DummyAccount.getSolde();
+			System.out.println("**************************");
+			System.out.println("Votre ami a maintenant :" + (receveur.getSolde() + " euros."));
+			System.out.println("**************************");
 		}
+	}
+	public void cr√©dit(CCourant receveur, double montant) {
+
+		receveur.solde = receveur.getSolde() + montant ;
+
+		System.out.println("Vous avez rajout√© : " +  montant + " euros sur votre compte.");
+		System.out.println("----------------------------");
+		System.out.println("Vous avez maintenant : " + (getSolde()) + " euros.");
+	}
+
 }
